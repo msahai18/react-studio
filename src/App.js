3a7ab82a8e6ff5cfa16939f1396afafa5301c78a
Michelle Sahai <michelle@sahai.com>
2022-11-09 18:39:17 -0500

    Need to figure out price

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,19 +10,27 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
+
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  
+  const addToCart = (itemName) => (
+    setCart([...cart, itemName]));
+ 
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Michelle's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+
+        <BakeryItem pindex = {index} pname ={item}  pprice = {item} pdescription = {item} pimage = {item} updateCart = {addToCart}/> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
+        {cart}
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,16 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem({pname, pindex, pprice, pdescription, pimage, updateCart}){
+    
+    
+    return(
+        <div>{pindex} {pname.name} {pprice.price} {pdescription.description} 
+        
+        <img style = {{width: 400, height: "auto"}} src={pimage.image} alt ={pimage.image}/>
+        <button onClick={() => updateCart(pname.name)}>Add to Cart</button>
+        </div>
+    )
+        
+    
+        
+}
